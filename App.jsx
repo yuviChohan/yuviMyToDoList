b2a93c3c0a8786f6e82687dc388feea319e1e244
Yuvraj Singh Chohan <130729925+yuviChohan@users.noreply.github.com>
2024-03-01 12:18:03 -0700

    Revert "Lab 3- Props and State Management"
    This reverts commit 6bd4f25004eae2476926e44aa119e3d7b057fbf8.

--- App.jsx
+++ App.jsx
@@ -3,29 +3,22 @@
  *
  * @format
  */
- 
-import React, {useState} from 'react';
+
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet} from 'react-native';
 import ToDoList from './components/ToDoList';
 import ToDoForm from './components/ToDoForm';
- 
- 
+
+
 function App() {
-   const [tasks, setTasks] =
-   useState([
-    'Do laundry',
-    'Go to gym',
-    'Walk dog'
-  ]);
- 
   return (
     <SafeAreaView>
-      <ToDoList tasks ={tasks} />
+      <ToDoList />
       <ToDoForm />
     </SafeAreaView>
   );
 }
- 
+
 export default App;
--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -7,23 +7,29 @@ import {
   View,
   Text
 } from 'react-native';
-
-function ToDoList({ tasks }) {
+function ToDoList(props) {
   return (
     <>
-      <SafeAreaView>
-        <ScrollView>
-          {tasks.map((task) => (
-            <Pressable key={task} style={[styles.task, styles.incomplete]}>
-              <Text style={styles.taskText}>{task}</Text>
-            </Pressable>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
+      <ScrollView>
+        <Pressable>
+          <View style={[styles.task, styles.completed]}>
+            <Text style={styles.taskText}>Do laundry</Text>
+          </View>
+        </Pressable>
+        <Pressable>
+          <View style={[styles.task]}>
+            <Text style={styles.taskText}>Go to gym</Text>
+          </View>
+        </Pressable>
+        <Pressable>
+          <View style={[styles.task, styles.completed]}>
+            <Text style={styles.taskText}>Walk dog</Text>
+          </View>
+        </Pressable>
+      </ScrollView>
     </>
   );
 }
-
 const styles = StyleSheet.create({
   task: {
     padding: 10,
